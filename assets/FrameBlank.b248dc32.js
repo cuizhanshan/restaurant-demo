ab00560b40f05f54e7385a1c8a679c8df9a08e8f
less is more <cuizhanshan1025@163.com>
2021-10-31 23:16:11 +0800

    init

--- assets/FrameBlank.b248dc32.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.de242ad9.js";import{A as a,B as n,D as r}from"./vendor.931bb567.js";const o=a({name:"FrameBlank"});function t(s,c,d,m,p,_){return n(),r("div")}var l=e(o,[["render",t]]);export{l as default};
--- assets/header.d801b988.js
+++ assets/header.d801b988.js
@@ -1,1 +1,1 @@
-var a="/assets/header.1b5fa5f8.jpg";export{a as h};
+var a="https://cuizhanshan.github.io/restaurant-demo/assets/header.1b5fa5f8.jpg";export{a as h};
