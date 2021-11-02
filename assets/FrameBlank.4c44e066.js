4f5b07cdae488cb86568f2f56b7416f9fd8b7b95
Ryan <cuizhanshan1025@163.com>
2021-11-02 09:08:05 +0800

    public path更换

---
+++ assets/FrameBlank.4c44e066.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.f6b546bc.js";import{A as n,B as r,D as a}from"./vendor.87297164.js";const o=n({name:"FrameBlank"});function t(s,c,m,p,f,_){return r(),a("div")}var l=e(o,[["render",t]]);export{l as default};
--- assets/header.d801b988.js
+++ assets/header.d801b988.js
@@ -1,1 +1,1 @@
-var a="https://cuizhanshan.github.io/restaurant-demo/assets/header.1b5fa5f8.jpg";export{a as h};
+var a="http://cuizs.xyz/assets/header.1b5fa5f8.jpg";export{a as h};
